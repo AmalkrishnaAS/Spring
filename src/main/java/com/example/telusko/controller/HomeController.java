e1996848d3f6149f5eeb7b50c18d58d41ec07fb3
Amalkrishna A S <amalkrishna02016@gmail.com>
2022-10-10 19:00:06 +0530

    delete and find

--- src/main/java/com/example/telusko/controller/HomeController.java
+++ src/main/java/com/example/telusko/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.telusko.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.telusko.dao.AlienRepo;
 import com.example.telusko.model.Alien;
@@ -20,5 +21,17 @@ public String homeString () {
 public String addAlien(Alien alien) {
 	repo.save(alien);
 	return "home";
+}
+@RequestMapping("/getAlien")
+public ModelAndView getAlien(int aid) {
+	Alien alien=repo.findById(aid).orElse(new Alien());
+	ModelAndView mv=new ModelAndView("show");
+	mv.addObject("alien", alien);
+	return mv;
+}
+@RequestMapping("DeleteAlien")
+public String DeleteAlien(int aid) {
+	repo.deleteById(aid);
+	return "Delete";
 }
 }
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -7,12 +7,35 @@
 <title>Home</title>
 </head>
 <body>
+<h2>
+Add
+</h2>
 <form action="addAlien">
 <input type="text" name="aname" /> <br>
 <input type="text" name="aid"><br>
 
 <br>
 <input type="submit">
+</form>
+<br>
+<br>
+<h2>
+Find
+</h2>
+<form action="getAlien">
+<input type="text" name="aid"><br>
+
+<br>
+<input type="submit">
+
+</form>
+
+<form action="DeleteAlien">
+<input type="text" name="aid"><br>
+
+<br>
+<input type="submit">
+
 </form>
 
 </body>
